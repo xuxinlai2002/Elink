8c2362a0ff1dba49aae97f9c4ea4dc1583188dce
xuxinlai2002 <xxl@local.com>
2022-07-06 18:20:28 +0800

    add start env

---
+++ eLinkContract/scripts/startEnv.js
@@ -0,0 +1,28 @@
+const { 
+    sleep ,
+    clearPostgres,
+    clearChainLink,
+    startPostgres,
+    startChainlink
+} = require('./helper')
+
+const main = async () => {
+
+    //clear postgres
+    await clearPostgres();
+    await sleep(2000);
+
+    //clear chainLink
+    await clearChainLink();
+    await sleep(2000);
+
+    //start postgres
+    await startPostgres();
+    await sleep(20000);
+
+    //start start chainlink
+    await startChainlink();
+   
+}
+ 
+main();

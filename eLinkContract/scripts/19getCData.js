920afe3de159626afc74113c54222c74268ca7a3
xuxinlai2002 <xxl@local.com>
2022-07-12 01:24:41 +0800

    add get data script

---
+++ eLinkContract/scripts/19getCData.js
@@ -0,0 +1,28 @@
+
+const { ethers, getChainId} = require('hardhat')
+const { readConfig,isTxSuccess} = require('./helper')
+
+const main = async () => {
+
+    console.log("6 get arbiter list ....");
+
+    let chainID = await getChainId();
+    let accounts = await ethers.getSigners()
+    let deployer = accounts[0];
+    console.log("chainID is :" + chainID + " address :" + deployer.address);
+
+    const DataConsumer__Contract = await ethers.getContractFactory('DataConsumer',deployer)
+
+    let DataConsumerAddress = await readConfig("5","DataConsumer");
+    console.log("xxl callback Test Address :",DataConsumerAddress);
+    
+    let DataConsumer = await DataConsumer__Contract.connect(deployer).attach(DataConsumerAddress);
+    
+    let resultData = await DataConsumer.data();
+    console.log("xxl result data ",resultData);
+
+}
+
+
+
+main();
